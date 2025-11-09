47292ac18b4cd3e90f4d4835906dda72d40eabf3
Yash Raturi <yashraturi70@gmail.com>
2025-11-09 21:28:05 +0530

    Lost my sanity trying to make it work on vercel now I have lost 😞

--- frontend/src/components/ProductPicker.tsx
+++ frontend/src/components/ProductPicker.tsx
@@ -18,8 +18,8 @@ const ProductPicker: React.FC = () => {
   useEffect(() => {
     const fetchProducts = async () => {
       try {
-        // const response = await fetch("http://localhost:5000/api/products");
-        const response = await fetch(`${import.meta.env.VITE_API_URL}/api/products`);
+        const response = await fetch("http://localhost:5000/api/products");
+        // const response = await fetch(`${import.meta.env.VITE_API_URL}/api/products`);
         const data = await response.json();
         setProducts(data);
         setLoading(false);
